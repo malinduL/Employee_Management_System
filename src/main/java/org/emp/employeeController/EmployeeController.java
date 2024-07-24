52319080f4940eff15796901a488c4c753e6c213
Malindu Lakshan <malindulakshan3002@gmail.com>
2024-07-25 02:42:36 +0530

    hibernate and | get All

--- src/main/java/org/emp/employeeController/EmployeeController.java
+++ src/main/java/org/emp/employeeController/EmployeeController.java
@@ -1,10 +1,11 @@
 package org.emp.employeeController;
 
+import lombok.RequiredArgsConstructor;
 import org.emp.dto.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.emp.entity.EmployeeEntity;
+import org.emp.service.EmployeeService;
+import org.emp.service.Impl.EmployeeServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/emp-controller")
 //http://localhost:8080/emp-controller/add-employee
+@RequiredArgsConstructor
 public class EmployeeController {
-    List<Employee> employeeList=new ArrayList();
+   EmployeeService service;
     @PostMapping("add-employee")
     public void addEmployee(@RequestBody Employee employee){
-          employeeList.add(employee);
+         service.addEmployee(employee);
+    }
+    @GetMapping("get-all")
+    public List<EmployeeEntity> getAll(){
+        return service.getAll();
     }
 }
